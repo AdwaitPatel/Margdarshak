95419b9aa06ab05a8e5be2c17b28e63cb2c41b40
Adwait Patel <145041038+AdwaitPatel@users.noreply.github.com>
2025-07-17 11:23:47 +0530

    Navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./components/Common/Navbar";
+
+function App() {
+  return (
+    <div className="bg-black h-[2200px]">
+      <Navbar />
+      <div className="flex-center text-5xl text-white pt-[400px]">
+        <h1>Find Your Perfect Career Path</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
