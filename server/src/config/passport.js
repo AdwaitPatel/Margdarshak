69defab80abac9ac3c90dd99c6fca2c3c6cdd882
Adwait Patel <145041038+AdwaitPatel@users.noreply.github.com>
2025-08-27 23:36:31 +0530

    deployed and changed the callback url

--- server/src/config/passport.js
+++ server/src/config/passport.js
@@ -11,7 +11,8 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "http://localhost:8000/api/v1/auth/google/callback",
+            callbackURL:
+                "https://margdarshak-kt73.onrender.com/api/v1/auth/google/callback",
         },
         async (accessToken, refreshToken, profile, done) => {
             try {
